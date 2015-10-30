2eef6fc8e721bbd342a106f492a9500d50a64eda
Alex Woroschilow <alex.woroschilow@gmail.com>
2015-10-30 11:26:03 +0100

    fix path

--- starter-planner.sh
+++ starter-planner.sh
@@ -33,7 +33,7 @@ signal_handler () {
 	DESTINATION_USER="wurst";
 	DESTINATION_HOST="flensburg.zbh.uni-hamburg.de";
 	DESTINATION_FLENSBURG="/home/other/wurst/wurst_rss/xml";
-	DESTINATION_FLENSBURG_LOG="${DESTINATION_FLENSBURG}${SCRIPT_LOG_REMOTE}";
+	DESTINATION_FLENSBURG_LOG="${DESTINATION_FLENSBURG}/${SCRIPT_LOG_REMOTE}";
 
 	echo "<?xml version=\"1.1\" encoding=\"UTF-8\" ?>" > ${SCRIPT_LOG_XML};
 	echo "<response>" >> ${SCRIPT_LOG_XML};
