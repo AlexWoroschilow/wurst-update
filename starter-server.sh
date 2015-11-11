2416a2bac96ede1fbda8a3f4cb12caf38cfed18e
Alex Woroschilow <alex.woroschilow@gmail.com>
2015-11-11 17:28:04 +0100

    remove server timeout, move to script defaults

--- starter-server.sh
+++ starter-server.sh
@@ -5,7 +5,6 @@
 #$ -S /bin/bash
 set -e
 
-SERVER_TIMEOUT="120"
 CONFIG_SERVER="$(pwd)/etc/server.conf";
 CONFIG_STARTER="$(pwd)/etc/starter.conf";
 CONFIG_LOGGER="$(pwd)/etc/logger.conf";
@@ -23,7 +22,7 @@ echo "Checking worker script..."
 check_file ${SCRIPT_WORKER};
 
 echo "Run: ${SCRIPT_SERVER}";
-${SCRIPT_SERVER} --configlog=${CONFIG_LOGGER} --configfile=${CONFIG_SERVER} --timeout=${SERVER_TIMEOUT} 1>>${SCRIPT_STD_OUT} 2>> ${SCRIPT_STD_ERR} &
+${SCRIPT_SERVER} --configlog=${CONFIG_LOGGER} --configfile=${CONFIG_SERVER} 1>>${SCRIPT_STD_OUT} 2>> ${SCRIPT_STD_ERR} &
 SERVER_PID=$!;
 echo "Server pid: ${SERVER_PID}";
 
