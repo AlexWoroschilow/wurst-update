66a210d5d4775951b0cbb99e379d791febe5444d
Alex Woroschilow <alex.woroschilow@gmail.com>
2015-11-13 12:47:03 +0100

    update starter scripts, run starter, worker, and server on same hosts, add extra workers to do jobs fast

--- starter.sh
+++ starter.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+#$ -clear
+#$ -cwd
+#$ -q stud.q
+#$ -S /bin/bash
 set -e
 
-CONFIG_STARTER="$(pwd)/etc/starter.conf";
 echo "Read config: ${CONFIG_STARTER}";
 . "/${CONFIG_STARTER}";
 
@@ -16,17 +19,89 @@ echo "Clean logs: ${SCRIPT_LOG_XML}";
 rm -f ${SCRIPT_LOG_XML};
 
 
+# Write a xml file with status
+# for php status-rss stream 
+signal_handler () {
 
-echo "Run: ${SCRIPT_STARTER_SERVER}";
-SERVER=$(qsub -S /bin/bash ${SCRIPT_STARTER_SERVER} | tr -d -c 0-9);
-echo "Server SGE Job id: ${SERVER}";
+	SERVER_PID=$1;
+	WORKER_PID=$2;
+	PLANNER_PID=$3;
+	SCRIPT_LOG_XML=$4;
+	SCRIPT_LOG_ALL=$5;
+	SCRIPT_STD_ERR=$6;
+	
+	TIMESTAMP="$(date +'%s')";
+	SCRIPT_LOG_REMOTE_XML="${TIMESTAMP}.xml";
+	SCRIPT_LOG_REMOTE_TXT="${TIMESTAMP}.log";
 
-#STARTER_PLANNER="qsub -S /bin/bash  ${SCRIPT_STARTER_PLANNER}"
-#echo "Run: ${STARTER_PLANNER}";
-#PLANNER=$(${STARTER_PLANNER} | tr -d -c 0-9)
-#echo "Planner SGE Job id: ${PLANNER}";
+	echo "<?xml version=\"1.1\" encoding=\"UTF-8\" ?>" > ${SCRIPT_LOG_XML};
+	echo "<response>" >> ${SCRIPT_LOG_XML};
+	echo "<task>wurst-update</task>" >> ${SCRIPT_LOG_XML};
+	echo "<date>$(date +%s)</date>" >> ${SCRIPT_LOG_XML};
+	echo "<status>${?}</status>" >> ${SCRIPT_LOG_XML};
+	echo "<logfile><![CDATA[${SCRIPT_LOG_REMOTE_TXT}]]></logfile>" >> ${SCRIPT_LOG_XML};
+	echo "<info><![CDATA[$(cat ${SCRIPT_LOG_ALL} | grep INFO | head -50)]]></info>" >> ${SCRIPT_LOG_XML};
+	echo "<warning><![CDATA[$(cat ${SCRIPT_LOG_ALL} | grep WARN | head -50)]]></warning>" >> ${SCRIPT_LOG_XML};
+	echo "<error><![CDATA[$(cat ${SCRIPT_LOG_ALL} | grep ERROR | head -50)]]></error>" >> ${SCRIPT_LOG_XML};
+	echo "<fatal><![CDATA[$(cat ${SCRIPT_LOG_ALL} | grep FATAL | head -50)]]></fatal>" >> ${SCRIPT_LOG_XML};
+	echo "<stderr><![CDATA[$(head -50 ${SCRIPT_STD_ERR})]]></stderr>" >> ${SCRIPT_LOG_XML};
+	echo "</response>" >> ${SCRIPT_LOG_XML};
 
-#STARTER_WORKER="qsub -S /bin/bash ${SCRIPT_STARTER_WORKER}"
-#echo "Run: ${STARTER_WORKER}";
-#${STARTER_WORKER} 1>>${SCRIPT_STD_OUT} 2>> ${SCRIPT_STD_ERR} &
+	DESTINATION_USER="wurst";
+	DESTINATION_HOST="flensburg.zbh.uni-hamburg.de";
 
+	DESTINATION_FLENSBURG="/home/other/wurst/wurst_rss/xml";
+	DESTINATION_FLENSBURG_XML="${DESTINATION_FLENSBURG}/${SCRIPT_LOG_REMOTE_XML}";
+	scp ${SCRIPT_LOG_XML} ${DESTINATION_USER}@${DESTINATION_HOST}:${DESTINATION_FLENSBURG_XML}
+
+	DESTINATION_FLENSBURG_RSS="/home/other/wurst/public_html/rss/log";
+	DESTINATION_FLENSBURG_TXT="${DESTINATION_FLENSBURG_RSS}/${SCRIPT_LOG_REMOTE_TXT}";	
+	cat ${SCRIPT_STD_ERR} >> ${SCRIPT_LOG_ALL};
+	scp ${SCRIPT_LOG_ALL} ${DESTINATION_USER}@${DESTINATION_HOST}:${DESTINATION_FLENSBURG_TXT}
+
+	ssh ${DESTINATION_USER}@${DESTINATION_HOST} chmod -R 777 ${DESTINATION_FLENSBURG}
+	
+	kill ${PLANNER_PID} ${WORKER_PID} ${SERVER_PID};
+	exit;
+}
+
+
+WORKER_PID=0;
+PLANNER_PID=0;
+SERVER_PID=0;
+
+# Run configurator script
+# needs to read configs from perl variables
+# and write current actual config for planner
+# with library, source, clusters and so on
+echo "Run: ${SCRIPT_CONFIGURATOR}";
+${SCRIPT_CONFIGURATOR} --config=${CONFIG_UPDATER} 1>>${SCRIPT_STD_OUT} 2>> ${SCRIPT_STD_ERR} &
+CONFIGURATOR_PID=$!;
+echo "Configurator pid: ${CONFIGURATOR_PID}";
+
+
+echo "Run: ${SCRIPT_SERVER}";
+${SCRIPT_SERVER} --config=${CONFIG_UPDATER} --logger=${CONFIG_LOGGER} 1>>${SCRIPT_STD_OUT} 2>> ${SCRIPT_STD_ERR} &
+SERVER_PID=$!;
+echo "Server pid: ${SERVER_PID}";
+
+# run worker here do do some  job even if 
+# other workers has not been started
+echo "Run worker: ${SCRIPT_WORKER}";
+${SCRIPT_WORKER} --config=${CONFIG_UPDATER} --logger=${CONFIG_LOGGER} 1>>${SCRIPT_STD_OUT} 2>> ${SCRIPT_STD_ERR} &
+WORKER_PID=$!;
+echo "worker pid: ${WORKER_PID}";
+
+echo "Run planner: ${SCRIPT_PLANNER}";
+${SCRIPT_PLANNER} --configlog=${CONFIG_LOGGER} --configfile=${CONFIG_SERVER} 1>>${SCRIPT_STD_OUT} 2>> ${SCRIPT_STD_ERR} &
+PLANNER_PID=$!;
+echo "Planner pid: ${PLANNER_PID}";
+
+# Kill only server process if this 
+# shell has bee closed with signal
+# Catch sytem signals needs to write 
+# a xml files for rss status stream
+trap 'signal_handler ${SERVER_PID} ${PLANNER_PID} ${WORKER_PID} ${SCRIPT_LOG_XML} ${SCRIPT_LOG_ALL} ${SCRIPT_STD_ERR};' EXIT KILL HUP INT TERM
+
+wait ${PLANNER_PID};
+exit;
