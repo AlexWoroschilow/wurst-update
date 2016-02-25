fb5117ec90614d8a6b4011cc998ab8ff0a7aafa2
Alex Woroschilow <alex.woroschilow@gmail.com>
2016-02-25 18:26:53 +0100

    =hostfix

--- bin/server.sh
+++ bin/server.sh
@@ -11,6 +11,11 @@
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#$ -clear
+#$ -cwd
+#$ -q stud.q
+#$ -S /bin/bash
+
 ROOT=$(pwd)
 echo "Folder: ${ROOT}"
 
